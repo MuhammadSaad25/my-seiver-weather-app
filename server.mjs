4068f186f9955c747d1a8e0b4ed98a63a3333410
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-11-27 20:41:22 +0500

    create const __dirname

--- server.mjs
+++ server.mjs
@@ -1,19 +1,22 @@
 // const express = require('express')
 console.log("i am server file");
-import express from 'express';
-import path from 'path'
+import express from "express";
+import path from "path";
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/abc', (req, res) => {
-    console.log("request ip:",req.ip);
-  res.send('Hello World' + new Date().toDateString())
-})
-app.use('/', (req, res) => {
-  app.use('/', express.static(path.join(path.resolve(__dirname), './web/build/index.html')))
-})
+app.get("/abc", (req, res) => {
+  console.log("request ip:", req.ip);
+  res.send("Hello World" + new Date().toDateString());
+});
+
+const __dirname = path.resolve();
+
+app.use("/", (req, res) => {
+  app.use("/", express.static(path.join(__dirname, "./web/build/index.html")));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
