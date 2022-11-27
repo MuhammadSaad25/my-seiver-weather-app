d9a474e4b1d2af4a44bfc86cbb83cde109f3b805
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-11-27 21:31:39 +0500

    one page web

--- server.mjs
+++ server.mjs
@@ -14,6 +14,8 @@ app.get("/abc", (req, res) => {
 const __dirname = path.resolve();
 app.use('/', express.static(path.join(__dirname, './web/build')))
 
+app.use('/*', express.static(path.join(__dirname, './web/build')))
+
 // app.use("/", express.static(path.join(__dirname, "./web/build/index.html")));
 
 app.listen(port, () => {
