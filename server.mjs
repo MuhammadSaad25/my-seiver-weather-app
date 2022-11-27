057ec1e58a9c3697f85b6f5d49bea4ecf3689ae3
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-11-28 04:19:53 +0500

    puch data

--- server.mjs
+++ server.mjs
@@ -2,23 +2,32 @@
 console.log("i am server file");
 import express from "express";
 import path from "path";
+import cors from "cors";
+// import apiData from "./data.json"
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/abc", (req, res) => {
   console.log("request ip:", req.ip);
   res.send("Hello World" + new Date().toDateString());
 });
 
+app.get("/data", (req, res) => {
+  // console.log("request ip:", req.ip);
+  res.send({
+    temp:30,
+    wind:40
+  });
+});
 
 const __dirname = path.resolve();
-app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use("/", express.static(path.join(__dirname, "./web/build")));
 
-
-
-
-app.use('/*', express.static(path.join(__dirname, './web/build')))
+app.use("*", express.static(path.join(__dirname, "./web/build")));
 
 // app.use("/", express.static(path.join(__dirname, "./web/build/index.html")));
 
--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,73 +1,15 @@
 import React from 'react'
+// import data from "../../server.mjs"
 
-const App = (city) => {
-
-  App.get(
-    `../../server.mjs=${city}`
-    // `https://api.weatherapi.com/v1/current.json?key=b52a1217af194e27bfa155504223006&q=${city}`
-  )
-    .then(function (response) {
-      // handle success
-      const data = response.data;
-
-      console.log(data);
-    }
-    )
-    .then(function (response) {
-      // handle success
-      const data = response.data;
-
-      console.log(data);
-    })
+const App = () => {
 
 
 
   return (
     <div>
+      {/* {data.temp} */}
 
-      <div class="nav_div">
-        <h1>Weather App</h1>
-        <form onsubmit="myFunction(); return false">
-          <input type="text" id="city" placeholder="Enter your City name" />
-        </form>
-
-      </div>
-      <div class="card">
-
-
-        <div class="result_div">
-
-          <div class="img_div" >
-            <img src="" alt="" id="weather_icon" />
-          </div>
-          <div class="temp_result">
-            <div id="tempC"></div>
-            <div id="tempF"></div>
-          </div>
-
-        </div>
-
-        <div class="sub">
-          <div class="left">
-            <div id="time"></div>
-            <div id="city_name"></div>
-            <div id="feels_like_c"></div>
-            <div id="humidity"></div>
-            <div id="wind_speed_kph"></div>
-
-          </div>
-
-          <div class="right">
-            <div id="last_update"></div>
-            <div id="weather_condition"></div>
-            <div id="feels_like_f"></div>
-            <div id="visibility"></div>
-            <div id="wind_speed_mph"></div>
-          </div>
-        </div>
-
-      </div>
-
+      <h1>hello</h1>
     </div>
   )
 }
