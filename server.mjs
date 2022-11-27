cd232e31e260d05f8d4f3be9aac92a7ae441b569
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-11-27 20:29:46 +0500

    push

--- server.mjs
+++ server.mjs
@@ -1,13 +1,17 @@
 // const express = require('express')
 console.log("i am server file");
 import express from 'express';
+import path from 'path'
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
     console.log("request ip:",req.ip);
   res.send('Hello World' + new Date().toDateString())
+})
+app.use('/', (req, res) => {
+  app.use('/', express.static(path.join(path.resolve(__dirname), './web/build/index.html')))
 })
 
 app.listen(port, () => {
