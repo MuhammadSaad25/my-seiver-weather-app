e5d754386266c17d291bbffec530483b3729dd28
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-11-28 05:39:22 +0500

    complete

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,7 +4,7 @@ import Weather from './weather'
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      {/* <h1>hello</h1> */}
       <Weather />
     </div>
   )
--- web/src/weather.jsx
+++ web/src/weather.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import data from "./data.json"
+import './weather.css'
 
 
 const weather = () => {
@@ -7,9 +8,59 @@ const weather = () => {
         <div>
             {data.map(data => {
                 return (
-                    <div>
-                        {data.temp}
+
+<div class="main">
+            <div class="nav_div">
+                <h1>Weather App</h1>
+                <form onsubmit="myFunction(); return false">
+                    <input type="text" id="city" placeholder="Enter your City name"/>
+                </form>
+
+            </div>
+
+
+            <div class="card">
+
+
+                <div class="result_div">
+
+                    <div class="img_div" >
+                        <img src="//cdn.weatherapi.com/weather/64x64/night/113.png" alt="" id="weather_icon"/>
                     </div>
+                    <div class="temp_result">
+                        <div id="tempC">{data.tempC}C</div>
+                        <div id="tempF">{data.tempF}F</div>
+                    </div>
+
+                </div>
+
+                <div class="sub">
+                    <div class="left">
+                        <div id="time">{new Date().toDateString()}</div>
+                        <div id="city_name">{data.city}</div>
+                        <div id="feels_like_c">Feels Like:{data.ftempC}C</div>
+                        <div id="humidity">Humidity:{data.humidity}%</div>
+                        <div id="wind_speed_kph">Wind Speed:{data.windKPH}KP/H</div>
+
+                    </div>
+
+                    <div class="right">
+                        <div id="last_update">{new Date().toDateString()}Last Update</div>
+                        <div id="weather_condition">{data.text}</div>
+                        <div id="feels_like_f">Feels Like:{data.ftempC}F</div>
+                        <div id="visibility">Visibility{data.visibility}KM</div>
+                        <div id="wind_speed_mph">Wind Speed:{data.windMPH}MP/H</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+                    // <div>
+                    //     {data.city}
+                    //     {data.temp}
+                    // </div>
                 )
             })}
         </div>
