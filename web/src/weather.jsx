353462492fab016821cc02183e840bd32ef3de47
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-11-28 12:36:18 +0500

    sent data

--- web/src/weather.jsx
+++ web/src/weather.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import data from "./data.json"
+// import data from "./data.json"
 import axios from "axios"
 import './weather.css'
 // import { useState } from 'react'
@@ -16,76 +16,77 @@ const weather = () => {
         })
 
     return (
-        <div>
-            {data.map(data => {
-                return (
+        <ul>
+            {
+                this.state.data
+                    .map(data =>
+                        <li>{data.tempC}</li>
+                    )
+            }
+        </ul>
 
-                    <div class="main">
+        // <div>
+        //     {data.map(data => {
+        //         return (
 
-                        <ul>
-                            {
-                                this.state.data
-                                    .map(data =>
-                                        <li>{data.tempC}</li>
-                                    )
-                            }
-                        </ul>
-
-
-                        <div class="nav_div">
-                            <h1>Weather App</h1>
-                            <form onsubmit="myFunction(); return false">
-                                <input type="text" id="city" placeholder="Enter your City name" />
-                            </form>
-
-                        </div>
+        //             <div class="main">
 
 
-                        <div class="card">
+
+        //                 <div class="nav_div">
+        //                     <h1>Weather App</h1>
+        //                     <form onsubmit="myFunction(); return false">
+        //                         <input type="text" id="city" placeholder="Enter your City name" />
+        //                     </form>
+
+        //                 </div>
 
 
-                            <div class="result_div">
-
-                                <div class="img_div" >
-                                    <img src="//cdn.weatherapi.com/weather/64x64/night/113.png" alt="" id="weather_icon" />
-                                </div>
-                                <div class="temp_result">
-                                    <div id="tempC">{data.tempC}C</div>
-                                    <div id="tempF">{data.tempF}F</div>
-                                </div>
-
-                            </div>
-
-                            <div class="sub">
-                                <div class="left">
-                                    <div id="time">{new Date().toDateString()}</div>
-                                    <div id="city_name">{data.city}</div>
-                                    <div id="feels_like_c">Feels Like:{data.ftempC}C</div>
-                                    <div id="humidity">Humidity:{data.humidity}%</div>
-                                    <div id="wind_speed_kph">Wind Speed:{data.windKPH}KP/H</div>
-
-                                </div>
-
-                                <div class="right">
-                                    <div id="last_update">{new Date().toDateString()}Last Update</div>
-                                    <div id="weather_condition">{data.text}</div>
-                                    <div id="feels_like_f">Feels Like:{data.ftempC}F</div>
-                                    <div id="visibility">Visibility{data.visibility}KM</div>
-                                    <div id="wind_speed_mph">Wind Speed:{data.windMPH}MP/H</div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
+        //                 <div class="card">
 
 
-                    // <div>
-                    //     {data.city}
-                    //     {data.temp}
-                    // </div>
-                )
-            })}
-        </div>
+        //                     <div class="result_div">
+
+        //                         <div class="img_div" >
+        //                             <img src="//cdn.weatherapi.com/weather/64x64/night/113.png" alt="" id="weather_icon" />
+        //                         </div>
+        //                         <div class="temp_result">
+        //                             <div id="tempC">{data.tempC}C</div>
+        //                             <div id="tempF">{data.tempF}F</div>
+        //                         </div>
+
+        //                     </div>
+
+        //                     <div class="sub">
+        //                         <div class="left">
+        //                             <div id="time">{new Date().toDateString()}</div>
+        //                             <div id="city_name">{data.city}</div>
+        //                             <div id="feels_like_c">Feels Like:{data.ftempC}C</div>
+        //                             <div id="humidity">Humidity:{data.humidity}%</div>
+        //                             <div id="wind_speed_kph">Wind Speed:{data.windKPH}KP/H</div>
+
+        //                         </div>
+
+        //                         <div class="right">
+        //                             <div id="last_update">{new Date().toDateString()}Last Update</div>
+        //                             <div id="weather_condition">{data.text}</div>
+        //                             <div id="feels_like_f">Feels Like:{data.ftempC}F</div>
+        //                             <div id="visibility">Visibility{data.visibility}KM</div>
+        //                             <div id="wind_speed_mph">Wind Speed:{data.windMPH}MP/H</div>
+        //                         </div>
+        //                     </div>
+
+        //                 </div>
+        //             </div>
+
+
+        // <div>
+        //     {data.city}
+        //     {data.temp}
+        // </div>
+    // )
+// })}
+        // </div >
     )
 }
 
