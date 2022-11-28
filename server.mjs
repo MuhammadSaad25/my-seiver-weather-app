da1e41c1586a6a535cf018af06edab68cedbaa13
Muhammad Saad <muhammadsaad2522@gmail.com>
2022-11-28 12:31:58 +0500

    sent data

--- server.mjs
+++ server.mjs
@@ -5,7 +5,6 @@ import path from "path";
 import cors from "cors";
 // import data from "./web//src/data.json"
 
-
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -18,7 +17,18 @@ app.get("/abc", (req, res) => {
 
 app.get("/weather", (req, res) => {
   // console.log("request ip:", req.ip);
-  res.send("./web/build");
+  res.send({
+    city: "Karachi",
+    text: "Clear",
+    tempC: 22,
+    tempF: 71,
+    ftempC: 20,
+    ftempF: 68,
+    humidity: 83,
+    visibility: "6KM",
+    windKPH: 6.8,
+    windMPH: 4.3,
+  });
 });
 
 const __dirname = path.resolve();
--- web/src/weather.jsx
+++ web/src/weather.jsx
@@ -1,60 +1,82 @@
 import React from 'react'
 import data from "./data.json"
+import axios from "axios"
 import './weather.css'
+// import { useState } from 'react'
+
 
 
 const weather = () => {
+    // const {data, setData} = setState({})
+
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+            const data = res.data;
+            this.setState({ data });
+        })
+
     return (
         <div>
             {data.map(data => {
                 return (
 
-<div class="main">
-            <div class="nav_div">
-                <h1>Weather App</h1>
-                <form onsubmit="myFunction(); return false">
-                    <input type="text" id="city" placeholder="Enter your City name"/>
-                </form>
+                    <div class="main">
 
-            </div>
+                        <ul>
+                            {
+                                this.state.data
+                                    .map(data =>
+                                        <li>{data.tempC}</li>
+                                    )
+                            }
+                        </ul>
 
 
-            <div class="card">
+                        <div class="nav_div">
+                            <h1>Weather App</h1>
+                            <form onsubmit="myFunction(); return false">
+                                <input type="text" id="city" placeholder="Enter your City name" />
+                            </form>
+
+                        </div>
 
 
-                <div class="result_div">
+                        <div class="card">
 
-                    <div class="img_div" >
-                        <img src="//cdn.weatherapi.com/weather/64x64/night/113.png" alt="" id="weather_icon"/>
+
+                            <div class="result_div">
+
+                                <div class="img_div" >
+                                    <img src="//cdn.weatherapi.com/weather/64x64/night/113.png" alt="" id="weather_icon" />
+                                </div>
+                                <div class="temp_result">
+                                    <div id="tempC">{data.tempC}C</div>
+                                    <div id="tempF">{data.tempF}F</div>
+                                </div>
+
+                            </div>
+
+                            <div class="sub">
+                                <div class="left">
+                                    <div id="time">{new Date().toDateString()}</div>
+                                    <div id="city_name">{data.city}</div>
+                                    <div id="feels_like_c">Feels Like:{data.ftempC}C</div>
+                                    <div id="humidity">Humidity:{data.humidity}%</div>
+                                    <div id="wind_speed_kph">Wind Speed:{data.windKPH}KP/H</div>
+
+                                </div>
+
+                                <div class="right">
+                                    <div id="last_update">{new Date().toDateString()}Last Update</div>
+                                    <div id="weather_condition">{data.text}</div>
+                                    <div id="feels_like_f">Feels Like:{data.ftempC}F</div>
+                                    <div id="visibility">Visibility{data.visibility}KM</div>
+                                    <div id="wind_speed_mph">Wind Speed:{data.windMPH}MP/H</div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
-                    <div class="temp_result">
-                        <div id="tempC">{data.tempC}C</div>
-                        <div id="tempF">{data.tempF}F</div>
-                    </div>
-
-                </div>
-
-                <div class="sub">
-                    <div class="left">
-                        <div id="time">{new Date().toDateString()}</div>
-                        <div id="city_name">{data.city}</div>
-                        <div id="feels_like_c">Feels Like:{data.ftempC}C</div>
-                        <div id="humidity">Humidity:{data.humidity}%</div>
-                        <div id="wind_speed_kph">Wind Speed:{data.windKPH}KP/H</div>
-
-                    </div>
-
-                    <div class="right">
-                        <div id="last_update">{new Date().toDateString()}Last Update</div>
-                        <div id="weather_condition">{data.text}</div>
-                        <div id="feels_like_f">Feels Like:{data.ftempC}F</div>
-                        <div id="visibility">Visibility{data.visibility}KM</div>
-                        <div id="wind_speed_mph">Wind Speed:{data.windMPH}MP/H</div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
 
 
                     // <div>
